0876100971b880af10b1866eae7b1afc8341eebe
gsylwia <113548942+gsylwia@users.noreply.github.com>
2022-09-28 21:28:09 +0200

    fix code and add validation

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const arr = [1, 2, 3, 4];
 
-function randomItem (arr) {
-	return arr[Math.floor(Math.random()*arr.length)];
+randomItem = (arr) => {
+	if (Array.isArray(arr) && arr.length != 0 && arr != null && arr != undefined) {
+		const randomNumber = Math.floor(Math.random()*arr.length);
+		return arr[randomNumber];
+	} else {
+		console.log("It's not an array!");
+	}
 }
 
 console.log(randomItem(arr));
